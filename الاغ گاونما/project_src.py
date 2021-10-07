90b7b45474fdee9a6cdd23586a47b72a96d18011
MohammadHR <77969281+mhr-rahimi@users.noreply.github.com>
2021-10-07 12:21:19 -0700

    Create project_src.py

---
+++ الاغ گاونما/project_src.py
@@ -0,0 +1,11 @@
+time=input();
+time=time.split();
+ararnum,mamanum,i=0;
+while i < int(time[0]) :
+   if i< int(time[0]):
+      i+=(int(time[1])+1);
+      ararnum +=1;
+   if i<int(int(time[0])):
+      i+=(int(time[2])+1);
+      mamanum+=1;
+print("%s %s"%(ararnum,mamanum));
